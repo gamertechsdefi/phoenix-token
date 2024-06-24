a92d887b9cfc75bc15ba56d98ddb7de47707416f
Gamer <gamertechs.defi@gmail.com>
2024-06-25 00:23:22 +0100

    updating...

--- src/app/page.js
+++ src/app/page.js
@@ -5,22 +5,22 @@ import Header from "./components/header";
 import "./css/fire-effect.css";
 import Tokenomics from "./components/tokenomics";
 
-
-const bot2 = "/images/bot2.png";
+const webpfp = "/images/website_pfp.png";
 
 export default function Home() {
   return (
     <body>
       <header>
-      <Header />
+        <Header />
       </header>
 
       <main className="px-8 md:px-16">
         {/* Intro section */}
-        <section
-          className="px-8 md:px-16 flex items-center justify-center h-screen md:h-screen text-2xl sm:text-3xl md:text-7xl text-center"
-        >
-          <div 
+        <section className="flex flex-col-reverse md:flex-row items-center h-screen md:h-full ">
+        <div className="flex md:w-[50%]">
+              <Image className="justify-center" src={webpfp} width={600} height={200} />
+            </div>
+          <div className="text-center m-auto md:text-left text-4xl md:text-7xl md:w-[50%]"
             style={{
               fontFamily: "Zen Dots",
             }}
@@ -30,10 +30,9 @@ export default function Home() {
         </section>
 
         {/* About Section */}
-        <section className="h-auto sm:h-screen ">
-          <div className="flex flex-col justify-between md:flex-row items-center">
-            <div className="flex-col w-full md:w-[50%]">
-              <div className="mb-4">
+        <section className="mt-16 h-auto sm:h-screen ">
+          <div className="flex flex-col gap-10 justify-between md:flex-row items-center">
+              <div className="bg-[#ff6f00]  p-4 rounded-[25px] mb-4 md:w-[50%]">
                 <h2 className="font-bold text-3xl pb-4">
                   Safeguarding Communities
                 </h2>
@@ -49,9 +48,9 @@ export default function Home() {
                 </p>
               </div>
 
-              <div className="mb-8 sm:mb-16">
+              <div className="bg-[#ff6f00] p-4 rounded-[25px] mb-4 md:w-[50%]">
                 <h2 className="font-bold text-3xl pb-4">
-                  Empowering Individuals
+                  Safeguarding Communities
                 </h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
@@ -65,11 +64,6 @@ export default function Home() {
                 </p>
               </div>
             </div>
-
-            <div className="flex md:width-[50%]">
-              <Image className="align-center" src={bot2} width="300px" />
-            </div>
-          </div>
         </section>
 
         {/*Utilities section */}
@@ -130,42 +124,50 @@ export default function Home() {
 
             {/*Phoenix Bot */}
             <div className="flex flex-col md:flex-row">
-            <div className="mr-16 bg-[#FF6B00] flex flex-col w-50 h-50 md:w-80 md:h-80 justify-center text-center rounded-full mb-4">
-              <p>TOAL SUPPLY</p>
-              <h2 className="font-bold">100M</h2>
-            </div>
-            <div>
-              <div className="bg-[red] w- rounded-[25px] p-4 flex flex-row justify-between gap-20">
-                <p>Foundation</p>
-                <p>20%</p>
+              <div className="mr-16 bg-[#FF6B00] flex flex-col w-50 h-50 md:w-80 md:h-80 justify-center text-center rounded-full mb-4">
+                <p>TOAL SUPPLY</p>
+                <h2 className="font-bold">100M</h2>
               </div>
-              
-              <div className="bg-[red] rounded-[25px] p-4 flex flex-row justify-between gap-20">
-                <p>Marketing & Development</p>
-                <p>20%</p>
-              </div>
-            </div>
-            </div>
+              <div>
+                <div className="bg-[red] w- rounded-[25px] p-4 flex flex-row justify-between gap-20">
+                  <p>Foundation</p>
+                  <p>20%</p>
+                </div>
 
+                <div className="bg-[red] rounded-[25px] p-4 flex flex-row justify-between gap-20">
+                  <p>Marketing & Development</p>
+                  <p>20%</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
-{/*Community Reward section */}
-        <section> 
+        {/*Community Reward section */}
+        <section>
           <div className="mt-16 h-full md:h-screen items-center">
-            <h1 className="text-2xl font-bold mb-4">
+            <h1 className="text-2xl font-bold mb-4" style={{color: "orange"}}>
               COMMUNITY REWARDS MECHANISM
             </h1>
 
             {/*Phoenix Bot */}
-            <div className="bg-[#FF6B00] p-8  rounded-[20px] mb-4">
-              <p className="text-sm pb-4">
+            <div className="bg-white bg-opacity-15  p-8  rounded-[20px] mb-4 hover:bg-opacity-25">
+              <p className="text-sm pb-4 z-100 ">
                 {" "}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco
+                Phoenix Token is bringing back the era for Community with a lot
+                of reward! $PHT is a deflationary token designed to become more
+                scarce over time. All holders of Phoenix Token will earn more
+                $WKC (WikiCat Coin) and is automatically sent to your wallet by
+                simply holding Phoenix Token coins in your wallet. Watch the
+                amount of WikiCat Coin grow in your wallet as Phoenix Token coin
+                holders automatically receive a 1% fee from every transaction
+                that happens on the Phoenix Token network. The community
+                receives more $WKC coins from the fees generated on each
+                transaction.
               </p>
             </div>
+
+
           </div>
         </section>
 
@@ -189,7 +191,6 @@ export default function Home() {
         </section>
 
         {/* <Tokenomics /> */}
-        
       </main>
     </body>
   );
