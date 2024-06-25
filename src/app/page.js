4ca009a3b8ba94645df4e9770cdeed711769dd95
Gamer <gamertechs.defi@gmail.com>
2024-06-25 10:54:17 +0100

    updating...

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { motion, useScroll } from "framer-motion";
+
 import Image from "next/image";
 import Header from "./components/header";
 import "./css/fire-effect.css";
@@ -8,6 +10,14 @@ import Tokenomics from "./components/tokenomics";
 const webpfp = "/images/website_pfp.png";
 
 export default function Home() {
+  const AboutPopUp = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
   return (
     <body>
       <header>
@@ -17,54 +27,77 @@ export default function Home() {
       <main className="px-8 md:px-16">
         {/* Intro section */}
         <section className="flex flex-col-reverse md:flex-row items-center h-screen md:h-full ">
-        <div className="flex md:w-[50%]">
-              <Image className="justify-center" src={webpfp} width={600} height={200} />
-            </div>
-          <div className="text-center m-auto md:text-left text-4xl md:text-7xl md:w-[50%]"
-            style={{
-              fontFamily: "Zen Dots",
-            }}
-          >
-            <h2>THE ORDINARY MANS TOKEN</h2>
+          <div className="flex md:w-[50%]">
+            <Image
+              className="justify-center"
+              src={webpfp}
+              width={600}
+              height={200}
+            />
+          </div>
+          <div className="md:w-[50%]">
+            <motion.h2
+            variants={AboutPopUp}
+            initial="hidden"
+            animate="visible"
+              style={{
+                fontFamily: "Zen Dots",
+              }}
+              className="text-center m-auto md:text-left text-4xl md:text-7xl "
+            >
+              THE ORDINARY MANS TOKEN
+            </motion.h2>
           </div>
         </section>
 
         {/* About Section */}
-        <section className="mt-16 h-auto sm:h-screen ">
+        <motion.section className="mt-16 h-screen sm:h-full ">
           <div className="flex flex-col gap-10 justify-between md:flex-row items-center">
-              <div className="bg-[#ff6f00]  p-4 rounded-[25px] mb-4 md:w-[50%]">
-                <h2 className="font-bold text-3xl pb-4">
-                  Safeguarding Communities
-                </h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </p>
-              </div>
-
-              <div className="bg-[#ff6f00] p-4 rounded-[25px] mb-4 md:w-[50%]">
-                <h2 className="font-bold text-3xl pb-4">
-                  Safeguarding Communities
-                </h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </p>
-              </div>
+            <div className="bg-[#ff6f00]  p-4 rounded-[25px] mb-4 md:w-[50%]">
+              <h2 className="font-bold text-3xl pb-4">
+                Safeguarding Communities
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
             </div>
-        </section>
+
+            <div className="bg-[#ff6f00] p-4 rounded-[25px] mb-4 md:w-[50%]">
+              <h2 className="font-bold text-3xl pb-4">
+                Safeguarding Communities
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+          <div className="bg-[#ff6f00] p-4 rounded-[25px] mb-4 ">
+            <h2 className="font-bold text-3xl pb-4">
+              Safeguarding Communities
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </motion.section>
 
         {/*Utilities section */}
         <section>
@@ -146,7 +179,7 @@ export default function Home() {
         {/*Community Reward section */}
         <section>
           <div className="mt-16 h-full md:h-screen items-center">
-            <h1 className="text-2xl font-bold mb-4" style={{color: "orange"}}>
+            <h1 className="text-2xl font-bold mb-4" style={{ color: "orange" }}>
               COMMUNITY REWARDS MECHANISM
             </h1>
 
@@ -166,8 +199,6 @@ export default function Home() {
                 transaction.
               </p>
             </div>
-
-
           </div>
         </section>
 
