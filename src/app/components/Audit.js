1adc12b458c6f284b4240f740c067110687b5250
Gamer <gamertechs.defi@gmail.com>
2024-12-07 03:08:00 +0100

    Update Audit.js

--- src/app/components/Audit.js
+++ src/app/components/Audit.js
@@ -8,7 +8,7 @@ export default function AuditPage(){
     return(
         <div className="px-8 md:px-16 pt-4 pb-24 md:py-16 grid grid-cols-1 md:grid-cols-2 items-center gap-8">
             <div>
-                <h1 className="text-2xl font-bold">We have been audited by</h1>
+                <h1 className="text-xl font-bold">We have been audited by</h1>
                 <Link href="https://skeletonecosystem.com/audit-phoenix-token-bep20/">
                 <Image src={skeletonLogo} width={300} alt="skeleton ecosystem logo" />
                 </Link>
