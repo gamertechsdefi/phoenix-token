620195c62dcd077cb712611b99454691d2d113b4
Gamer <gamertechs.defi@gmail.com>
2025-07-18 14:45:41 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter, Zen_Dots, Space_Mono, Work_Sans} from "next/font/google";
 import "./globals.css";
+import Analytics from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 const spacemono = Space_Mono({subsets: ["latin"], weight: "400"});
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <link rel="stylesheet" href="https://voltichange.net/css/widget.css" />
       </head>
       <body className={spacemono.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
